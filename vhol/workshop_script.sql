5c9c649f93f238c881a7885b18bdb3a5d1bd110b
Jie-Hong, Lim <jie-hong.lim@snowflake.com>
2022-04-18 17:34:39 +0800

    Added steps

--- vhol/workshop_script.sql
+++ vhol/workshop_script.sql
@@ -1,5 +1,5 @@
 
---- Create a table 
+--- Step 1 Create a table 
 create or replace table trips
 (tripduration integer,
 starttime timestamp,
@@ -19,7 +19,7 @@ birth_year integer,
 gender integer);
 
 
---- Create a file format for data loading
+--- Step 2 Create a file format for data loading
 create or replace file format csv type='csv'
   compression = 'auto' field_delimiter = ',' record_delimiter = '\n'
   skip_header = 0 field_optionally_enclosed_by = '\042' trim_space = false
@@ -27,42 +27,43 @@ create or replace file format csv type='csv'
   date_format = 'auto' timestamp_format = 'auto' null_if = ('') comment = 'file format for ingesting data for zero to snowflake';
 
 
---- Create an external stage to an existing open S3 bucket
+--- Step 3.1 Create an external stage to an existing open S3 bucket
 create or replace stage citibike_trips
     url = 's3://snowflake-workshop-lab/citibike-trips/';
 
--- Execute a directory listing
+-- Step 3.2 Execute a directory listing
 list @citibike_trips pattern='.*csv.*';
 
+-- Step 4.1 Copying from S3 into Snowflake table 
 -- ~1 minute 370 files
 copy into trips from @citibike_trips file_format=CSV pattern = '.*csv.*';
 
--- 61 M rows
+-- Step 4.2 Check row count ~61 M rows
 select count (*) from trips;
 
--- let's redo the loading
+-- Step 4.3 let's redo the loading
 truncate table trips;
 
--- Switching from a XSmall single node to a Medium 4 nodes cluster
+-- Step 4.4 Switching from a XSmall single node to a Medium 4 nodes cluster
 -- Replace XXXX with your team name and team number
 use warehouse team_XXXX_XXXX_2_medium_wh;
 
--- ~30 seconds 370 files
+-- Step 4.5 Copying with a bigger resource ~30 seconds 370 files
 copy into trips from @citibike_trips file_format=CSV pattern = '.*csv.*';
 
--- Checking 61 M rows
+-- Step 4.6 Check row count 
 select count(*) from trips;
 
--- Getting a sample of the trips data
+-- Step 5.1 Getting a sample of the trips data
 select * from trips limit 20;
 
--- Sample query
+-- Step 5.2 Sample query
 select start_station_name, end_station_name, count(*) as trips_count from trips 
 group by start_station_name, end_station_name
 order by trips_count desc
 limit 5;
 
--- Let's switch over to our datasets
+-- Step 6 Let's switch over to our datasets
 use database ml_workspace;
 
 select * from search_trends_google;
